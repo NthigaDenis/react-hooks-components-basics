ddf9fd8c3cbc24fc8ddfe5817a2d2df4dc25252b
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-09-19 22:16:26 +0300

    Completed Code Along

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
       <Comment />
     </div>
   );
+   fnction Article() {
+    return (
+      <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    );
+  }
+  function Comment() {
+    return <div>Naturally, I agree with this article.</div>;
+  }
 }
-
 export default App;
